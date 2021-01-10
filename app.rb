afce608a1a8f84c58889edc924e6eeff1918f657
mrsdo <me@martidolce.com>
2021-01-10 18:14:22 -0500

    views links???

--- app.rb
+++ app.rb
@@ -6,12 +6,16 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require_relative 'config/environments'
 
+configure do
+  set :public_folder, 'public'
+  set :views, '/views'
+end
 
 get '/' do
   erb :start
 end
 
-get '/views/authentication' do
+get '/authentication' do
   erb :index
 end
 
