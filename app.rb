eefd45db31dc45d1979581f057b07f6094193362
mrsdo <me@martidolce.com>
2021-01-10 18:03:51 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -6,8 +6,11 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require_relative 'config/environments'
 
-
+class ApplicationController < Sinatra::Base
   get '/' do
     erb :start
   end
-
+  # get '/views/authentication' do
+  #   erb :index
+  # end
+end
